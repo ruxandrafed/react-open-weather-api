6c415832cab2e71e7033c5d342da368e0242cf15
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-01-17 21:06:08 -0800

    Move Hello World into Main; update app to use react router

---
+++ app/containers/Main.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+
+var Main = (props) =>
+  (<div>
+    Hello World!
+  </div>);
+
+module.exports = Main;
--- app/index.js
+++ app/index.js
@@ -1,12 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var HelloWorld = (props) =>
-  (<div>
-    Hello World!
-  </div>);
+var routes = require('./config/routes');
 
 ReactDOM.render(
-  <HelloWorld />,
+  routes,
   document.getElementById('app')
 );
