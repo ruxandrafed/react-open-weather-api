45e453785c25d24cb4f1d7fa41c3be95ce827bc5
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-01-22 16:57:17 -0800

    Dump all passed state on the detail page

--- app/containers/DayDetailContainer.js
+++ app/containers/DayDetailContainer.js
@@ -1,10 +1,12 @@
 var React = require('react');
 var DayDetail = require('../components/DayDetail');
+var puke = require('../utils/otherHelpers').puke;
 
 var DayDetailContainer = React.createClass({
   render: function () {
     return (
-      <DayDetail />
+      /* <DayDetail /> */
+      <div>{puke(this.props.state)}</div>
     )
   }
 });
